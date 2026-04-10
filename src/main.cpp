e6b55fe125b7bddd8217173519800544e0e63a12
nganchiho <nganchihon@gmail.com>
2026-04-10 09:34:17 +0800

    code for testing hc12+lcd display

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,22 @@
 #include <Arduino.h>
 #include <mcp2515.h>
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 static constexpr uint8_t RPM_PERIOD = 20;
 
 can_frame rx_frame;
+
+// Custom Char
+byte degCelsius[8] = { // degree celsius char
+  0b01000,
+  0b10100,
+  0b01000, 
+  0b00011, 
+  0b00100, 
+  0b00100, 
+  0b00100, 
+  0b00011  
+};
 
 // Define pin assignment using actual Arduino pin numbers
 // SPI Pins for CAN Controllers (MCP2515)
@@ -18,34 +31,37 @@ can_frame rx_frame;
 #define CAN1_CS_PIN PIN_PB1 ///< @brief Chip Select pin for CAN Controller 1 (PB1)
 
 // HC-12 Wireless Serial Communication Module Pins
-#define HC12_RX_PIN 0  ///< @brief Receive pin for HC-12 module (PD0 - Serial RX)
-#define HC12_TX_PIN 1  ///< @brief Transmit pin for HC-12 module (PD1 - Serial TX)
-#define HC12_SET_PIN 8 ///< @brief SET pin for HC-12 module (PB0)
+#define HC12_RX PIN_PD0  ///< @brief Receive pin for HC-12 module (PD0 - Serial RX)
+#define HC12_TX PIN_PD1  ///< @brief Transmit pin for HC-12 module (PD1 - Serial TX)
+#define HC12_SET PIN_PB0 ///< @brief SET pin for HC-12 module (PB0)
+SoftwareSerial HC12(HC12_RX,HC12_TX);
 
 // Rotary Encoder Pins
-#define ENC_A_PIN A0 ///< @brief Pin for Rotary Encoder A output (PC0 - Analog 0)
-#define ENC_B_PIN A1 ///< @brief Pin for Rotary Encoder B output (PC1 - Analog 1)
+#define ENC_A PIN_PC0 ///< @brief Pin for Rotary Encoder A output (PC0 - Analog 0)
+#define ENC_B PIN_PC1 ///< @brief Pin for Rotary Encoder B output (PC1 - Analog 1)
+int lastStateA , lastStateB;
 
 // I2C Communication Pins
-#define SDA_PIN A4 ///< @brief SDA pin for I2C communication (PC4 - Analog 4)
-#define SCL_PIN A5 ///< @brief SCL pin for I2C communication (PC5 - Analog 5)
+#define SDA_PIN PIN_PC4 ///< @brief SDA pin for I2C communication (PC4 - Analog 4)
+#define SCL_PIN PIN_PC5 ///< @brief SCL pin for I2C communication (PC5 - Analog 5)
 
 // General Purpose Input/Output (GPIO) Pins
-#define GPIO_1_PIN A2 ///< @brief General Purpose I/O Pin 1 (PC2 - Analog 2)
-#define GPIO_2_PIN A3 ///< @brief General Purpose I/O Pin 2 (PC3 - Analog 3)
-#define GPIO_3_PIN 7  ///< @brief General Purpose I/O Pin 3 (PD7 - Digital 7)
-#define GPIO_4_PIN 2  ///< @brief General Purpose I/O Pin 4 (PD2 - Digital 2)
-#define GPIO_5_PIN 3  ///< @brief General Purpose I/O Pin 5 (PD3 - Digital 3)
-#define GPIO_6_PIN 4  ///< @brief General Purpose I/O Pin 6 (PD4 - Digital 4)
-#define GPIO_7_PIN 5  ///< @brief General Purpose I/O Pin 7 (PD5 - Digital 5)
-#define GPIO_8_PIN 6  ///< @brief General Purpose I/O Pin 8 (PD6 - Digital 6)
+#define GPIO_1 PIN_PC2 ///< @brief General Purpose I/O Pin 1 (PC2 - Analog 2)
+#define GPIO_2 PIN_PC3 ///< @brief General Purpose I/O Pin 2 (PC3 - Analog 3)
+#define GPIO_3 PIN_PD7  ///< @brief General Purpose I/O Pin 3 (PD7 - Digital 7)
+#define GPIO_4 PIN_PD2  ///< @brief General Purpose I/O Pin 4 (PD2 - Digital 2)
+#define GPIO_5 PIN_PD3  ///< @brief General Purpose I/O Pin 5 (PD3 - Digital 3)
+#define GPIO_6 PIN_PD4  ///< @brief General Purpose I/O Pin 6 (PD4 - Digital 4)
+#define GPIO_7 PIN_PD5  ///< @brief General Purpose I/O Pin 7 (PD5 - Digital 5)
+#define GPIO_8 PIN_PD6  ///< @brief General Purpose I/O Pin 8 (PD6 - Digital 6)
+
 
 /**
  * @brief An array containing all the GPIO pins to be tested.
  */
 const int gpioPins[] = {
-	GPIO_1_PIN, GPIO_2_PIN, GPIO_3_PIN, GPIO_4_PIN,
-	GPIO_5_PIN, GPIO_6_PIN, GPIO_7_PIN, GPIO_8_PIN};
+	GPIO_1, GPIO_2, GPIO_3, GPIO_4,
+	GPIO_5, GPIO_6, GPIO_7, GPIO_8};
 MCP2515 can_vcu(CAN0_CS_PIN);
 MCP2515 can_ssru(CAN1_CS_PIN);
 static constexpr canid_t VCU_READ = 0x181; /**< Motor read CAN ID */
@@ -103,8 +119,9 @@ uint8_t write_counter = 0;
 
 void setup()
 {
-	// Initialize Serial communication at 9600 baud rate for debugging output.
-	Serial.begin(115200);
+	// Initialize Serial communication at 9600 baud rate for debugging output.	
+    Serial.begin(9600);
+    HC12.begin(9600);
 	lcd.begin(20, 4);
 
 	can_vcu.reset();
@@ -149,24 +166,25 @@ void loop()
 	lcd.print(millis());
 
 	MCP2515::ERROR read_state = can_vcu.readMessage(&rx_frame);
-
-	Serial.write("State: ");
+	Serial.print("State: ");
 	Serial.write(static_cast<char>(read_state)+'a');
 	Serial.write("\n");
 
 	if (read_state == MCP2515::ERROR_OK)
 	{
-		Serial.write("ID: ");
-		Serial.write(rx_frame.can_id+'0');
-		Serial.write("\t DLC: ");
-		Serial.write(rx_frame.can_dlc+'0');
-		Serial.write("\t Data: ");
-		for (int i = 0; i < 8; ++i)
+		Serial.print("ID: 0x");
+		Serial.print(rx_frame.can_id, HEX);
+		Serial.print("\tDLC: ");
+		Serial.print(rx_frame.can_dlc);
+		Serial.print("\tData: ");
+		for (int i = 0; i < rx_frame.can_dlc && i < 8; ++i)
 		{
-			Serial.write(rx_frame.data[i]+'0');
-			Serial.write("\t");
+			if (rx_frame.data[i] < 0x10)
+				Serial.print("0");
+			Serial.print(rx_frame.data[i], HEX);
+			Serial.print(" ");
 		}
-		Serial.write("\n");
+		Serial.println();
 
 		lcd.setCursor(0, 0);
 		lcd.print("ID:        ");
@@ -181,7 +199,6 @@ void loop()
 			if (rx_frame.data[i] < 0x10)
 				lcd.print("0");
 			lcd.print(rx_frame.data[i], HEX);
-			lcd.print(" ");
 		}
 	}
 }
